0eaf815f0e2ca7bde27047ee935765c5b96d5834
Kenny <kennethpavia@gmail.com>
2022-06-20 04:40:13 -0600

    trying to fix an error

--- core/settings/production.py
+++ core/settings/production.py
@@ -5,7 +5,6 @@ import dj_database_url
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
-DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get(
-    os.environ.get('HEROKU_POSTGRESQL_MAROON_URL')))
+DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get('HEROKU_POSTGRESQL_MAROON_URL'))
 DEFAULT_CONNECTION.update({'CONN_MAX_AGE': 600})
 DATABASES = {'default': DEFAULT_CONNECTION}

95f277d25606a607ce673ceefa4dcea7d638b470
Kenny <kennethpavia@gmail.com>
2022-06-20 06:53:41 -0600

    fixed DEBUG so now it works as an env variable

--- core/settings/production.py
+++ core/settings/production.py
@@ -3,9 +3,8 @@ import dj_database_url
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
-print(type(DEBUG))
-print(DEBUG)
+DEBUG = bool(int(os.environ.get('DEBUG')))
+
 
 DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get('HEROKU_POSTGRESQL_MAROON_URL'))
 DEFAULT_CONNECTION.update({'CONN_MAX_AGE': 600})
